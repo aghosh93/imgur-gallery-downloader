48e9decf8bf8f3f50fc9581a782832333c278278
Anupam Ghosh <anupam.ghosh93@gmail.com>
2018-03-06 23:14:51 -0500

    Added commander and can now download through cli

--- download.js
+++ download.js
@@ -2,9 +2,17 @@ const albumImages = require('./lib/albumImages');
 const directories = require('./lib/directories');
 const downloadImages = require('./lib/downloadImages');
 
-let hash = 'ONTjY';
+let program = require('commander');
+program
+  .usage('<hashes...>')
+  .parse(process.argv);
 
-Promise.resolve()
-.then(() => directories.mkDirs(hash))
-.then(() => albumImages(hash))
-.then(downloadImages);
+console.log(program.args);
+if (program.args.length === 0) { program.help(); }
+
+Promise.resolve().then(program.args.reduce(function (chain, hash) {
+  return chain
+    .then(() => directories.mkDirs(hash))
+    .then(() => albumImages(hash))
+    .then(downloadImages);
+}, Promise.resolve()));
