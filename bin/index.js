3b2ee9fb18320f1e3d948d143ab764d0a298269e
Anupam Ghosh <anupam.ghosh93@gmail.com>
2018-03-14 23:57:20 -0400

    Fixing bin

--- bin/index.js
+++ bin/index.js
@@ -1,18 +1,23 @@
-const albumImages = require('./lib/albumImages');
-const directories = require('./lib/directories');
-const downloadImages = require('./lib/downloadImages');
+#!/usr/bin/env node
+
+const albumImages = require('../lib/albumImages');
+const directories = require('../lib/directories');
+const downloadImages = require('../lib/downloadImages');
 
 let program = require('commander');
 program
   .usage('<hashes...>')
   .parse(process.argv);
 
-console.log(program.args);
 if (program.args.length === 0) { program.help(); }
 
 Promise.resolve().then(program.args.reduce(function (chain, hash) {
   return chain
     .then(() => directories.mkDirs(hash))
     .then(() => albumImages(hash))
-    .then(downloadImages);
+    .then(downloadImages)
+    .catch(() => {
+      console.error(`An Error has occured with album hash: ${hash}`);
+      return Promise.resolve(); // Needed to continue the promise chain.
+    });
 }, Promise.resolve()));
